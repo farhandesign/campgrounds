55606d9ee9a6cad50e4426f70c66397fec99abea
farhandesign <farhan.design@outlook.com>
2020-11-20 06:08:25 +0100

    Display Reviews/ Delete Reviews / Campground Delete Middleware

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post(
 app.get(
 	'/campgrounds/:id',
 	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
+		const campground = await Campground.findById(req.params.id).populate('reviews');
 		res.render('campgrounds/show', { campground });
 	})
 );
@@ -126,6 +126,16 @@ app.post(
 		await review.save();
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+app.delete(
+	'/campgrounds/:id/reviews/:reviewId',
+	catchAsync(async (req, res) => {
+		const { id, reviewId } = req.params;
+		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+		await Review.findByIdAndDelete(reviewId);
+		res.redirect(`/campgrounds/${id}`);
 	})
 );
 
