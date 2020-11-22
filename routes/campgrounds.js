6f5ba00b9aed9ce146cdd221da28f011066fc026
farhandesign <farhan.design@outlook.com>
2020-11-22 07:21:02 +0100

    AUTHENTICATION: configured passport, register user, login, logout

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
+
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 
@@ -23,18 +25,19 @@ router.get(
 	})
 );
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
 	res.render('campgrounds/new');
 });
 
 router.post(
 	'/',
+	isLoggedIn,
 	validateCampground,
 	catchAsync(async (req, res, next) => {
 		// if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 		const campground = new Campground(req.body.campground);
 		await campground.save();
-		req.flash('success', 'Successfully made a new Campgrounds');
+		req.flash('success', 'Successfully made a new campground');
 
 		res.redirect(`/campgrounds/${campground._id}`);
 	})
@@ -54,6 +57,7 @@ router.get(
 
 router.get(
 	'/:id/edit',
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		if (!campground) {
@@ -66,6 +70,7 @@ router.get(
 
 router.put(
 	'/:id',
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const { id } = req.params;
 		const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
@@ -76,6 +81,7 @@ router.put(
 
 router.delete(
 	'/:id',
+	isLoggedIn,
 	catchAsync(async (req, res) => {
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
