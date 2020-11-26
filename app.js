13e5a9feff3e94bd78ba3a29dd74b3cdbf7bc906
farhandesign <farhan.design@outlook.com>
2020-11-26 10:37:15 +0100

    Ready to Deploy

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -13,14 +12,19 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-const { nextTick, getMaxListeners } = require('process');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const MongoDBStore = require('connect-mongo')(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/campgrounds';
 // CONNECT DB
-mongoose.connect('mongodb://localhost:27017/campgrounds', {
+mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
@@ -44,19 +48,83 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
+const store = new MongoDBStore({
+	url: dbUrl,
+	secret,
+	touchAfter: 24 * 60 * 60
+});
+
+store.on('error', function(e) {
+	console.log('SESSTION STORE ERROR', e);
+});
 
 const sessionConfig = {
-	secret: 'thisshouldbeabettersecret',
+	store,
+	name: 'session',
+	secret,
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
 		httpOnly: true,
+		// secure: true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7
 	}
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
+	'https://cdn.jsdelivr.net'
+];
+
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.mapbox.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/'
+];
+
+const connectSrcUrls = [
+	'https://api.mapbox.com/',
+	'https://a.tiles.mapbox.com/',
+	'https://b.tiles.mapbox.com/',
+	'https://events.mapbox.com/'
+];
+
+const fontSrcUrls = [];
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: [ "'self'", ...connectSrcUrls ],
+			scriptSrc: [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+			styleSrc: [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+			workerSrc: [ "'self'", 'blob:' ],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/dnvxsefyc/',
+				'https://images.unsplash.com/'
+			],
+			fontSrc: [ "'self'", ...fontSrcUrls ]
+		}
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -70,12 +138,6 @@ app.use((req, res, next) => {
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error');
 	next();
-});
-
-app.get('/fakeUser', async (req, res) => {
-	const user = new User({ email: 'colt@gmail.com', username: 'colt' });
-	const newUser = await User.register(user, 'chicken');
-	res.send(newUser);
 });
 
 app.use('/', userRoutes);
